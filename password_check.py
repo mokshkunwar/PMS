b04c26035d8d522644d33aba253d4c0355e7479f
Tushar Seth <Tushar.Seth@jda.com>
2021-01-26 22:54:30 +0530

    adding config changes for password validation

--- password_check.py
+++ password_check.py
@@ -1,3 +1,5 @@
+import configparser
+
 import pyhibp
 from pyhibp import pwnedpasswords as pw
 from flask import render_template
@@ -5,21 +7,25 @@ import bcrypt, re
 import datetime
 import pandas as pd
 
+config = configparser.ConfigParser(interpolation=None)
+config.read('./utils/config.ini')
+
+
 def validate_password(password):
-    if len(password) < 8 or re.search("\s", password) \
-            or not re.search("[a-z]", password) \
-            or not re.search("[A-Z]", password) \
-            or not re.search("[0-9]", password) \
-            or not re.search("[@!#$&%*]", password):
-        criteria_satisfied = False
-    else:
-        criteria_satisfied = True
-    return criteria_satisfied
+    password_min_chars = int(config.get('PASSWORD', 'CHAR_COUNT'))
+    required_chars = config.get('PASSWORD', 'REQUIRED_CHARS').split(',')
+    if len(password) < password_min_chars or re.search('\s', password):
+        return False
+    for each_required_chars in required_chars:
+        if not re.search(each_required_chars, password):
+            return False
+    return True
+
 
 def check_pawned_password(password):
     pyhibp.set_user_agent(ua="HIBP Application/0.0.1")
-    resp = pw.is_password_breached(password=password)
-    return resp
+    return pw.is_password_breached(password=password)
+
 
 def hash_password(password):
     # encrypt user entered password
@@ -27,6 +33,7 @@ def hash_password(password):
     salt = bcrypt.gensalt(12)
     hashed_password = bcrypt.hashpw(raw_password, salt)
     return hashed_password, salt
+
 
 def save_password(hashed_password, salt, username, system):
     date = datetime.datetime.now()
@@ -37,21 +44,19 @@ def save_password(hashed_password, salt, username, system):
         else:
             df2.to_csv('password.csv', mode='a', header=False, index=False)
 
+
 def match_password(hashed_password, password):
     hashed_password = hashed_password.replace('b\'', '').replace('\'', '')
-    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
-        return True
-    return False
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+
 
 def all_checks(password, confirm_password):
+    pawned_pass_limit = int(config.get('PASSWORD', 'PAWNED_PASSWORD_LIMIT'))
     if password != confirm_password:
         error = "Passwords Don't Match"
-        return error
-    criteria_satisfied = validate_password(password)
-    if criteria_satisfied == False:
+    if not validate_password(password):
         error = "Password did not mach with the criteria, please try with new password"
-        return error
-    response = check_pawned_password(password)
-    if response > 10:
+    if check_pawned_password(password) > pawned_pass_limit:
         error = "This password is very common, please try with new password"
-        return error
+    return error
+
